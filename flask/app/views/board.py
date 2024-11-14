0642b8f51e76fcc367a688069ee95ba3cd6a0df5
JSLEE <dlwotjd258@daum.net>
2024-11-14 17:51:19 +0900

    CSS 및 백엔드 수정

--- flask/app/views/board.py
+++ flask/app/views/board.py
@@ -7,7 +7,7 @@ from flask import render_template, url_for, redirect, request, session, current_
 from flask_login import login_required, current_user
 from openpyxl import Workbook, load_workbook
 from app.utils.db import execute_query
-
+from flask import jsonify
 
 bp = Blueprint('board', __name__)
 
@@ -23,7 +23,7 @@ def check_board_owner(func):# 완료
         return func(*args, **kwargs)
     return wrapper
 
-
+ 
 @bp.route("/list", methods=['GET'])
 @login_required
 def board_list(): #완료
@@ -39,7 +39,7 @@ def board_list(): #완료
     offset = (page - 1) * per_page
 
     count_query = r"SELECT COUNT(*) num FROM board"
-    query = r"SELECT id, title, content, created_at FROM board"
+    query = r"SELECT id, (select name from users where id = u_id) as uname, title, content, created_at FROM board"
     param = []
 
     if keyword:
@@ -118,7 +118,7 @@ def board_edit(idx: int):
         title = request.form.get('title')
         content = request.form.get('content')
 
-        query = r"UPDATE board SET title=%s, content=%s, WHERE id=%s"
+        query = r"UPDATE board SET title=%s, content=%s WHERE id=%s"
         param = (title, content, idx)
         execute_query(query, param, True)
 
@@ -141,5 +141,5 @@ def board_edit(idx: int):
 @check_board_owner
 def board_delete(idx: str):
     query = r"DELETE FROM board WHERE id=%s"
-    execute_query(query, (idx), True)
-    return redirect(url_for('board.board_list'))
+    execute_query(query, (idx,), True)
+    return redirect(url_for('board.board_list', deleted="true"))
